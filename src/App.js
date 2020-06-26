fb12e254e95494e655e5b6421f2fa9e4cac519dd
Mark Tomlinson <mark.tomlinson3@gmail.com>
2020-06-26 13:45:02 +1000

    added what did you do today input

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Habit Tracker</h1>
+        <form>
+          <label>
+            What did you do today?
+          <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form> 
       </header>
     </div>
   );
